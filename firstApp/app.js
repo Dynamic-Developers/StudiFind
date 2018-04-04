123397bce11a43f30e88be638d54a1da2ecb770e
Emma Kenny <emmakenny13@gmail.com>
2018-04-04 13:40:39 +0100

    Ad pages

--- firstApp/app.js
+++ firstApp/app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -33,6 +34,7 @@ app.use(function(req, res, next) {
 });
 
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -44,5 +46,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
+app.get('/contact', routes.contact);
 module.exports = app;
--- firstApp/routes/index.js
+++ firstApp/routes/index.js
@@ -4,6 +4,12 @@ var Comment = require('../models/comments');
 var User = require('../models/users');
 var jwt = require('jsonwebtoken');
 var formidable = require('formidable');
+var Contact = require('../models/contact');
+const nodemailer = require('nodemailer');
+
+var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,7 +24,8 @@ router.get('/feed', function(req, res, next) {
         var jwtString = req.cookies.Authorization.split(" ");
         var profile = verifyJwt(jwtString[1]);
         if (profile) {
-            res.render('feed');
+         		 res.render('feed');
+
         }
     }catch (err) {
             res.json({
@@ -29,13 +36,97 @@ router.get('/feed', function(req, res, next) {
             });
         }
 });
+router.get('/book', function(req, res, next) {
+
+   	 
+      Contact.find({}, function(err, contacts)
+            {
+            res.render('book', {contact : contacts});    
+            });
+
+        
+    
+        
+});
+
+router.get('/shoe', function(req, res, next) {
+
+   	 
+      Contact.find({}, function(err, contacts)
+            {
+            res.render('shoe', {contact : contacts});    
+            });
+
+        
+    
+        
+});
+
+router.get('/clothes', function(req, res, next) {
+
+   	 
+      Contact.find({}, function(err, contacts)
+            {
+            res.render('clothes', {contact : contacts});    
+            });
+
+        
+    
+        
+});
+
+router.get('/electronic', function(req, res, next) {
+
+   	 
+      Contact.find({}, function(err, contacts)
+            {
+            res.render('electronic', {contact : contacts});    
+            });
+
+        
+    
+        
+});
+
+router.get('/notes', function(req, res, next) {
+
+   	 
+      Contact.find({}, function(err, contacts)
+            {
+            res.render('notes', {contact : contacts});    
+            });
+
+        
+    
+        
+});
+
+router.get('/grinds', function(req, res, next) {
+
+   	 
+      Contact.find({}, function(err, contacts)
+            {
+            res.render('grinds', {contact : contacts});    
+            });
+
+        
+    
+        
+});
+
 router.get('/contact', function(req, res, next) {
 
+    req.param.id;
+    
     try {
         var jwtString = req.cookies.Authorization.split(" ");
         var profile = verifyJwt(jwtString[1]);
         if (profile) {
-            res.render('contact');
+            Contact.find({category:req.param.id}, function(err, contacts)
+            {
+            res.render('contact', {contact : contacts});    
+            });
+            
         }
     }catch (err) {
             res.json({
@@ -45,6 +136,37 @@ router.get('/contact', function(req, res, next) {
                 ]
             });
         }
+});
+
+router.get('/contactus', function(req, res, next) {
+  res.render('contactus', { title: 'Express' });
+});
+
+app.post('/contactus', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: GMAIL_USER,
+    subject: 'New message from contact form at tylerkrys.ca',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-failure');
+    }
+    else {
+      res.render('contact-success');
+    }
+  });
 });
 router.post('/addComment',function(req,res,next){
     
@@ -101,15 +223,41 @@ router.delete('/removeComment/:id', function(req, res, next){
 
 
 
-/*
- Verifies a JWT
- */
+
+router.post('/addContact',function(req,res,next){
+    
+	contact = new Contact(req.body);
+	contact.save(function (err, savedContact) {
+		if (err)
+			throw err;
+        
+        res.json(
+		// res.status(301).redirect("http://danu7.it.nuigalway.ie:8625")
+		);
+	});
+
+});
+router.get('/getContact', function(req,res,next) {
+    Contact.find({},function(err,contacts)
+                 {
+        if(err)
+            res.send(err);
+        
+        res.json(contacts);
+        
+    })
+});
+
+var hbs = require('hbs');
+hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+    
+
 function verifyJwt(jwtString) {
 
     var value = jwt.verify(jwtString, 'CSIsTheWorst');
     return value;
 }
-
-
 
 module.exports = router;
